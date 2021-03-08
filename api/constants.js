7910eb84cbe48e3961084306432e1705c91b1827
akramsaouri <ak.saouri@gmail.com>
2021-03-08 16:14:42 +0100

    why tf envs are hard on rn?

--- api/constants.js
+++ api/constants.js
@@ -1,4 +1,2 @@
-import { API_BASE_URL, ADD_REASON_ID } from "@env";
-
-export const apiBaseURL = API_BASE_URL;
-export const addReasonId = ADD_REASON_ID;
+export const apiBaseURL = process.env.API_BASE_URL;
+export const addReasonId = process.env.ADD_REASON_ID;
